4b71595c9dd14ac00267885fc6a0c5c8399251f7
PawelKozy <46841983+PawelKozy@users.noreply.github.com>
2023-01-16 17:42:54 +0100

    Update GetAllCustomTrustedLocations.ps1

--- GetAllCustomTrustedLocations.ps1
+++ GetAllCustomTrustedLocations.ps1
@@ -3,7 +3,7 @@ $officeApps = @("Word", "Excel", "PowerPoint", "Outlook")
 $defaultLocations = @("C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Office",
                       "C:\Program Files (x86)\Microsoft Office",
                       "C:\Program Files\Microsoft Office")
-
+                      
 foreach ($app in $officeApps) {
     $key = "HKCU:\Software\Microsoft\Office\16.0\$app\Security\Trusted Locations"
     if(Test-Path $key){
@@ -16,9 +16,12 @@ foreach ($app in $officeApps) {
                 $TrustedLocations += (Get-ItemProperty -Path $locationKey.PSPath).Path
             }
             $CustomLocations = Compare-Object -ReferenceObject $defaultLocations -DifferenceObject $TrustedLocations | Select-Object -ExpandProperty InputObject
-            foreach($location in $CustomLocations){
-                Write-Host "`n$i. $location" -ForegroundColor Green
-          $i++
+            if($CustomLocations.Count -gt 0){
+                foreach($location in $CustomLocations){
+                    Write-Host "`n$location" -ForegroundColor Green
+                }
+            }else{
+                Write-Host "No custom trusted locations found for $app"
             }
         } else {
             Write-Host "No Trusted Locations found for $app"
