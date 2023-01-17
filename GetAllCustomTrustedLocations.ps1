3b24972e116eb88871582711f8b892c284bbbb46
PawelKozy <46841983+PawelKozy@users.noreply.github.com>
2023-01-17 13:07:42 +0100

    Update GetAllCustomTrustedLocations.ps1

--- GetAllCustomTrustedLocations.ps1
+++ GetAllCustomTrustedLocations.ps1
@@ -3,24 +3,29 @@ foreach($userProfile in $userProfiles)
 {
     $user = $userProfile.LocalPath
     $officeApps = @("Word", "Excel", "PowerPoint", "Outlook")
-    $TrustedLocations = @()
     foreach ($app in $officeApps) {
         $key = "HKCU:\Software\Microsoft\Office\16.0\$app\Security\Trusted Locations"
         if(Test-Path $key){
             $locationKeys = Get-ChildItem -Path $key | Where-Object { $_.PSChildName -match "Location*" }
             if ($locationKeys) {
+                $TrustedLocations = @()
                 foreach($locationKey in $locationKeys){
                     $TrustedLocations += (Get-ItemProperty -Path $locationKey.PSPath).Path
                 }
-            } 
-        } 
-    }
-    if($TrustedLocations.Count -gt 0){
-        Write-Host "`nUser: $user" -ForegroundColor Cyan
-        foreach($location in $TrustedLocations){
-            Write-Host "`nTrusted Location: $location" -ForegroundColor Green
+                if($TrustedLocations.Count -gt 0){
+                    Write-Host "`nUser: $user `nApplication: $app" -ForegroundColor Cyan
+                    foreach($location in $TrustedLocations){
+                        Write-Host "`nTrusted Location: $location" -ForegroundColor Green
+                    }
+                }else{
+                    Write-Host "No trusted locations found for $app for user $user"
+                }
+            } else
+            {
+                Write-Host "No Trusted Locations found for $app for user $user"
+            }
+        } else {
+            Write-Host "No Trusted Locations key found for $app for user $user"
         }
-    }else{
-        Write-Host "No trusted locations found for user $user"
     }
 }
