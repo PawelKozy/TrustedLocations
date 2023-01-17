cf92bdc276eef0560aa19c581e790c27db7027b7
PawelKozy <46841983+PawelKozy@users.noreply.github.com>
2023-01-17 09:53:00 +0100

    Update GetAllCustomTrustedLocations.ps1

--- GetAllCustomTrustedLocations.ps1
+++ GetAllCustomTrustedLocations.ps1
@@ -1,21 +1,20 @@
-$officeApps = @("Word", "Excel", "PowerPoint", "Outlook")
 $user = (Get-WmiObject -Class Win32_UserProfile | Where-Object { $_.Special -eq $false }).LocalPath
-$defaultLocations = @("C:\Users\$user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Office",
+$officeApps = @("Word", "Excel", "PowerPoint", "Outlook")
+$defaultLocations = @("$user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Office",
                       "C:\Program Files (x86)\Microsoft Office",
                       "C:\Program Files\Microsoft Office")
-                      
 foreach ($app in $officeApps) {
-    $key = "HKLM:\Software\Microsoft\Office\16.0\$app\Security\Trusted Locations"
+    $key = "HKCU:\Software\Microsoft\Office\16.0\$app\Security\Trusted Locations"
     if(Test-Path $key){
         $locationKeys = Get-ChildItem -Path $key | Where-Object { $_.PSChildName -match "Location*" }
-        Write-Host "`nTrusted Locations for ${app}:" -ForegroundColor Cyan
+        Write-Host "`nTrusted Locations for $app:" -ForegroundColor Cyan
         if ($locationKeys) {
             $i = 1
             $TrustedLocations = @()
             foreach($locationKey in $locationKeys){
                 $TrustedLocations += (Get-ItemProperty -Path $locationKey.PSPath).Path
             }
-            $CustomLocations = Compare-Object -ReferenceObject $defaultLocations -DifferenceObject $TrustedLocations | Select-Object -ExpandProperty InputObject
+            $CustomLocations = $TrustedLocations | Where-Object {$defaultLocations -notcontains $_}
             if($CustomLocations.Count -gt 0){
                 foreach($location in $CustomLocations){
                     Write-Host "`n$location" -ForegroundColor Green
